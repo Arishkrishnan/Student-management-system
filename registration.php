228c7defb8f965e8bc53a847958030da46d1c75d
Arish-krishnan <32954595+Arishkrishnan@users.noreply.github.com>
2018-08-08 23:19:16 +0530

    php wrong

--- registration.php
+++ registration.php
@@ -7,28 +7,34 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'registration');
+$db = mysqli_connect('localhost', 'root', '', 'sms');
+
+if ($db->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
   $username = mysqli_real_escape_string($db, $_POST['username']);
+  $userid= mysqli_real_escape_string($db, $_POST['userid']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $mobile = mysqli_real_escape_string($db, $_POST['mobile']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password2']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
+ /* if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }*/
   if ($password_1 != $password_2) {
 	array_push($errors, "The two passwords do not match");
   }
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM registration WHERE username='$username' OR email='$email' or userid='$userid' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -37,6 +43,11 @@ if (isset($_POST['reg_user'])) {
       array_push($errors, "Username already exists");
     }
 
+      
+     if ($user['userid'] === $userid) {
+      array_push($errors, "Userid already exists");
+    }
+      
     if ($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
@@ -44,7 +55,7 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = $password_1;//encrypt the password before saving in the database if needed by using md5
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
@@ -52,5 +63,9 @@ if (isset($_POST['reg_user'])) {
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
   	header('location: index.php');
+    print("Successfully registerd!");
   }
+
 }
+?>
+
